b8d66ef59e0757fdd9fc19b069ebf71ee653c12b
Jeff Clegg <jeffclegg@att.net>
2020-12-16 14:04:05 -0500

    added schema, seed data, index.handlebars & main.handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES
+    ("Cowboy Burger", false), 
+    ("The Madlove Burger", false),
+    ("Black & Bleu Burger", false), 
+    ("The Southern Charm Burger", false),
+    ("Smoke & Pepper Burger", false),
+    ("Whiskey River BBQ Burger", false);
