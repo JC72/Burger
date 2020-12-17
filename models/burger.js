6364978a6a57ca478478915693bbfa4afda11065
Jeff Clegg <jeffclegg@att.net>
2020-12-16 21:29:09 -0500

    modified burger.js & burgers.js

---
+++ models/burger.js
@@ -0,0 +1,31 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var tableName = "burgers"
+
+var burger = {
+  all: function(cb) {
+    orm.all(tableName, function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create(tableName, cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update(tableName, objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete(tableName, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
