4271706ddda60b378deb844ae73a9e512effe22d
Jeff Clegg <jeffclegg@att.net>
2020-12-16 21:03:56 -0500

    change burger_style.js to burger_style.css

--- config/orm.js
+++ config/orm.js
@@ -41,7 +41,7 @@ function printQuestionMarks(num) {
   
   // Object for all our SQL statement functions.
   var orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
         if (err) {
@@ -50,7 +50,7 @@ function printQuestionMarks(num) {
         cb(result);
       });
     },
-    create: function(table, cols, vals, cb) {
+    insertOne: function(table, cols, vals, cb) {
       var queryString = "INSERT INTO " + table;
   
       queryString += " (";
@@ -71,7 +71,7 @@ function printQuestionMarks(num) {
       });
     },
     // An example of objColVals would be {name: panther, sleepy: true}
-    update: function(table, objColVals, condition, cb) {
+    updateOne: function(table, objColVals, condition, cb) {
       var queryString = "UPDATE " + table;
   
       queryString += " SET ";
